a28676cecd8210438c197e6ff44b38f365d7032e
sosinaesayas <sosinaesayas@gmail.com>
2022-02-05 17:50:00 -0300

    Feat: Interceptor Loggin

---
+++ src/shared/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse();
+    const request = context.getRequest();
+    const status = exception.getStatus
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const errorResponse = {
+      code: status,
+      timestamp: new Date().toLocaleDateString(),
+      path: request.url,
+      method: request.method,
+      message:
+        status !== HttpStatus.INTERNAL_SERVER_ERROR
+          ? exception.message || exception.message || null
+          : 'Internal server error',
+    };
+
+    response.status(status).json(errorResponse);
+  }
+}
