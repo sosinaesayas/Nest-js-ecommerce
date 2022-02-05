5b367545991937b885469a3edde73e5c2e0be258
sosinaesayas <sosinaesayas@gmail.com>
2022-02-05 17:47:16 -0300

    Feat: Auth Service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      sub: user._doc._id,
+      username: user._doc.username,
+      createdAt: user._doc.createdAt,
+    };
+
+    return {
+      acces_token: this.jwtService.sign(payload),
+    };
+  }
+}
