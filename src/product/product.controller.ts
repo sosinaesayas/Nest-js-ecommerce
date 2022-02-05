b71eac3d209068583438a6e9dac89dcefc3f355a
sosinaesayas <sosinaesayas@gmail.com>
2022-02-05 18:10:00 -0300

    Feat: Product Controller

---
+++ src/product/product.controller.ts
@@ -0,0 +1,108 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+  // UseGuards,
+} from '@nestjs/common';
+
+import { Product } from './interfaces/product.interface';
+import { CreateProductDTO, UpdateProductDTO } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+// @UseGuards(JwtAuthGuard)
+@Controller('product')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post()
+  async create(
+    @Res() res,
+    @Body() createProductDTO: CreateProductDTO,
+  ): Promise<Product> {
+    try {
+      const product = await this.productService.createProduct(createProductDTO);
+      return res.status(HttpStatus.OK).json({
+        message: 'Product created successfully',
+        product,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Get()
+  async findAll(@Res() res): Promise<Product[]> {
+    try {
+      const products = await this.productService.findAll();
+      return res.status(HttpStatus.OK).json({
+        message: 'Products obtained successfully',
+        products,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Get(':productID')
+  async read(
+    @Res() res,
+    @Param('productID') productID: string,
+  ): Promise<Product> {
+    try {
+      const product = await this.productService.findById(productID);
+      return res.status(HttpStatus.OK).json({
+        message: 'Product obtained successfully',
+        product,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Put(':productID')
+  async update(
+    @Res() res,
+    @Param('productID') productID: string,
+    @Body() updateProductDTO: UpdateProductDTO,
+  ): Promise<Product> {
+    try {
+      const product = await this.productService.updateProduct(
+        productID,
+        updateProductDTO,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Product update successfully',
+        product,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Delete(':productID')
+  async delete(
+    @Res() res,
+    @Param('productID') productID: string,
+    @Body() updateProductDTO: UpdateProductDTO,
+  ): Promise<Product> {
+    try {
+      const product = await this.productService.updateProduct(
+        productID,
+        updateProductDTO,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Product removed successfully',
+        product,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
