08aca1061f1d7a64ee45fddb9820dc1e1f13f184
sosinaesayas <sosinaesayas@gmail.com>
2022-02-06 16:18:05 -0300

    Add category property on my product schema

--- src/product/schemas/product.schema.ts
+++ src/product/schemas/product.schema.ts
@@ -3,6 +3,7 @@ import { Schema } from 'mongoose';
 export const ProductSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
   state: { type: Boolean, default: true },
