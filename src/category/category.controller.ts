0f49ab9bdc45de040d855cc14ffec754fe9c5713
sosinaesayas <sosinaesayas@gmail.com>
2022-02-06 16:15:45 -0300

    Feat: Category Controller

---
+++ src/category/category.controller.ts
@@ -0,0 +1,110 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import {
+  CreateCategoryDTO,
+  UpdateCategoryDTO,
+} from './dto/create-category.dto';
+import { Category } from './interfaces/category.interface';
+
+@Controller('category')
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Post()
+  async create(
+    @Res() res,
+    @Body() createCategoryDTO: CreateCategoryDTO,
+  ): Promise<Category> {
+    try {
+      const category = await this.categoryService.createCategory(
+        createCategoryDTO,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Category created successfully',
+        category,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Get()
+  async findAll(@Res() res): Promise<Category[]> {
+    try {
+      const category = await this.categoryService.findAll();
+      return res.status(HttpStatus.OK).json({
+        message: 'Categories obtained successfully',
+        category,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Get(':categoryID')
+  async findById(
+    @Res() res,
+    @Param('categoryID') categoryID: string,
+  ): Promise<Category> {
+    try {
+      const category = await this.categoryService.findById(categoryID);
+      return res.status(HttpStatus.OK).json({
+        message: 'Category obtained successfully',
+        category,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Put(':categoryID')
+  async update(
+    @Res() res,
+    @Param('categoryID') categoryID: string,
+    @Body() updateCategoryDTO: UpdateCategoryDTO,
+  ): Promise<Category> {
+    try {
+      const category = await this.categoryService.updateCategory(
+        categoryID,
+        updateCategoryDTO,
+      );
+      return res.status(HttpStatus.OK).json({
+        message: 'Category update successfully',
+        category,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Delete(':categoryID')
+  async delete(
+    @Res() res,
+    @Param('categoryID') categoryID: string,
+    @Body() updateCategoryDTO: UpdateCategoryDTO,
+  ): Promise<Category> {
+    try {
+      const category = await this.categoryService.updateCategory(
+        categoryID,
+        updateCategoryDTO,
+      );
+
+      return res.status(HttpStatus.OK).json({
+        message: 'Category removed successfully',
+        category,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
