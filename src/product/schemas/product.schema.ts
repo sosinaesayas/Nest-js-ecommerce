bab30a9d154de2d37c6a1e8c34da1c9d92509d48
sosinaesayas <sosinaesayas@gmail.com>
2022-02-09 14:32:38 -0300

    Fixed produt schema

--- src/product/schemas/product.schema.ts
+++ src/product/schemas/product.schema.ts
@@ -6,6 +6,8 @@ export const ProductSchema = new Schema({
   category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
+  comment: { type: Schema.Types.ObjectId, ref: 'Comment' },
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
   state: { type: Boolean, default: true },
   created: {
     type: Date,
