0f131e917f7ab4620dec7f43442d2b46ece713d5
sosinaesayas <sosinaesayas@gmail.com>
2022-02-09 22:21:53 -0300

    Feat: Sub category module

---
+++ src/subcategory/subcategory.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SubcategoryController } from './subcategory.controller';
+import { SubcategoryService } from './subcategory.service';
+
+@Module({
+  controllers: [SubcategoryController],
+  providers: [SubcategoryService]
+})
+export class SubcategoryModule {}
