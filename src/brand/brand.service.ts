d73d9f403e3d8c996a0c3bf2f551b1db5d0c521c
sosinaesayas <sosinaesayas@gmail.com>
2022-02-09 22:20:54 -0300

    Feat: Brand Service

---
+++ src/brand/brand.service.ts
@@ -0,0 +1,76 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateBrandDTO, UpdateBrandDTO } from './dto/create-brand.dto';
+import { Brand } from './interfaces/brand.interface';
+
+@Injectable()
+export class BrandService {
+  constructor(@InjectModel('Brand') private brandModel: Model<Brand>) {}
+
+  async findAll(): Promise<Brand[]> {
+    try {
+      return await this.brandModel.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async findById(id: string): Promise<Brand> {
+    try {
+      const brand = await this.brandModel.findById(id);
+
+      if (!brand) {
+        throw new HttpException('Brand not found', HttpStatus.NO_CONTENT);
+      }
+
+      return brand;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async createBrand(createBrandDTO: CreateBrandDTO): Promise<Brand> {
+    try {
+      const brand = await this.brandModel.create(createBrandDTO);
+      return await brand.save();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async updateBrand(
+    brandID: string,
+    updateBrandDTO: UpdateBrandDTO,
+  ): Promise<Brand> {
+    try {
+      const brand = await this.brandModel.findByIdAndUpdate(
+        brandID,
+        { new: true },
+        updateBrandDTO,
+      );
+
+      await brand.update(updateBrandDTO);
+      return await this.brandModel.findById(brandID);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async deleteBrand(
+    brandID: string,
+    updateBrandDTO: UpdateBrandDTO,
+  ): Promise<Brand> {
+    try {
+      const brand = await this.brandModel.findByIdAndUpdate(
+        brandID,
+        { state: false },
+        updateBrandDTO,
+      );
+
+      return brand;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
