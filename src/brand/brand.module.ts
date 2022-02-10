10db27b2954a4cd44617f01de1910923e2d5faa8
sosinaesayas <sosinaesayas@gmail.com>
2022-02-10 11:36:22 -0300

    Add Mongoose forFeature on my Modules

--- src/brand/brand.module.ts
+++ src/brand/brand.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SharedModule } from 'src/shared/shared.module';
 import { BrandController } from './brand.controller';
 import { BrandService } from './brand.service';
+import { BrandSchema } from './schemas/brand.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Brand', schema: BrandSchema }]),
+    SharedModule,
+  ],
   controllers: [BrandController],
   providers: [BrandService],
 })
--- src/subcategory/subcategory.module.ts
+++ src/subcategory/subcategory.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SharedModule } from 'src/shared/shared.module';
+import { SubCategorySchema } from './schemas/subcategory.schema';
 import { SubcategoryController } from './subcategory.controller';
 import { SubcategoryService } from './subcategory.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'SubCategory', schema: SubCategorySchema },
+    ]),
+    SharedModule,
+  ],
   controllers: [SubcategoryController],
   providers: [SubcategoryService],
 })
