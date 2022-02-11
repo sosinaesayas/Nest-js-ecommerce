e12a1832c9d62e7bd29b2cd6ed3c7b1d2d98ee77
sosinaesayas <sosinaesayas@gmail.com>
2022-02-10 23:47:12 -0300

    Add brand and subcategory in my CreateProductDTO

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -2,7 +2,9 @@ export class CreateProductDTO {
   title: string;
   image: string;
   description: string;
+  brand: string;
   category: string;
+  subcategory: string;
   comment: string;
   price: number;
   state: boolean;
