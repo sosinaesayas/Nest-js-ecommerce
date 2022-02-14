b1485e20b4d3c7416a386b810d07c98dcce58f78
sosinaesayas <sosinaesayas@gmail.com>
2022-02-14 16:47:32 -0300

    Add JwtAuthGuard on all Order CRUD

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,26 +1,22 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateOrderDTO } from './dto/create-order.dto';
 import { Order } from './interfaces/order.interface';
 import { OrderService } from './order.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Get()
-  // @UseGuards(AuthGuard('jwt'))
-  getOrders(): Promise<Order[]> {
-    return this.orderService.getOrders();
+  async getOrders(): Promise<Order[]> {
+    return await this.orderService.getOrders();
   }
 
   @Post()
-  // @UseGuards(AuthGuard('jwt'))
-  createOrder(
-    @Body() order: CreateOrderDTO,
-    // @UserDecorator() { id }: User,
-  ): Promise<Order> {
-    const newOrder = this.orderService.createOrder(order);
+  async createOrder(@Body() order: CreateOrderDTO): Promise<Order> {
+    const newOrder = await this.orderService.createOrder(order);
     console.log(newOrder);
     return newOrder;
   }
