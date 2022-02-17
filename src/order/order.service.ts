c47ba7f2540f7eef4828f7b4e13b864a725f1763
sosinaesayas <sosinaesayas@gmail.com>
2022-02-17 15:00:37 -0300

    Fixed populate and totalPrice value

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Product } from 'src/product/interfaces/product.interface';
 import { CreateOrderDTO } from './dto/create-order.dto';
 import { Order } from './interfaces/order.interface';
 
@@ -14,26 +15,23 @@ export class OrderService {
     return await this.orderModel.find();
   }
 
-  async createOrder(
-    createOrderDTO: CreateOrderDTO,
-    // userId: string,
-  ): Promise<any> {
+  async createOrder(createOrderDTO: CreateOrderDTO): Promise<Order> {
     try {
-      const { products } = createOrderDTO;
-      console.log(products);
-
       const newOrder = await this.orderModel.create(createOrderDTO);
+      const populatedOrder: CreateOrderDTO<Product> = await newOrder.populate({
+        path: 'products',
+        populate: {
+          path: 'product',
+        },
+      });
 
-      // const totalPrice = newOrder.products.reduce((acc, product) => {
-      //   const price = product.product.price * product.quantity;
-      //   return acc + price;
-      // }, 0);
-
-      // await newOrder.update({ totalPrice });
-
+      const totalPrice = populatedOrder.products.reduce((acc, product) => {
+        const price = product.product.price * product.quantity;
+        return acc + price;
+      }, 0);
+      newOrder.totalPrice = totalPrice;
       return await newOrder.save();
     } catch (error) {
-      console.log(error);
       throw new Error(error);
     }
   }
