2617dfc1f52961053488c50a69c5ede6b6c12987
sosinaesayas <sosinaesayas@gmail.com>
2022-02-17 14:59:33 -0300

    Fixed Product Order with Generic Type

--- src/order/interfaces/order.interface.ts
+++ src/order/interfaces/order.interface.ts
@@ -1,13 +1,12 @@
-import { Product } from 'src/product/interfaces/product.interface';
-import { User } from 'src/user/interfaces/user.interface';
+// import { User } from 'src/user/interfaces/user.interface';
 
-export interface ProductOrder {
-  product: Product;
+export interface ProductOrder<ProductType = string> {
+  product: ProductType;
   quantity: number;
 }
 
 export interface Order extends Document {
-  owner: User['_id'];
+  // owner: User['_id'];
   products: ProductOrder[];
   totalPrice: number;
   created: Date;
