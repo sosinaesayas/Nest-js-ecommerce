fa2ca2332951e09516ab64a9c77123c6165298d3
sosinaesayas <sosinaesayas@gmail.com>
2022-02-24 14:56:11 -0300

    Fixed: Implements decorators in Schema 'Category'

--- src/category/schema/category.schema.ts
+++ src/category/schema/category.schema.ts
@@ -1,10 +1,18 @@
-import { Schema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const CategorySchema = new Schema({
-  name: { type: String, required: true },
-  state: { type: Boolean, default: true },
-  created: {
-    type: Date,
-    default: Date.now,
-  },
-});
+export type CategorySchema = Category & Document;
+
+@Schema()
+export class Category {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: Boolean, default: true })
+  state: boolean;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
