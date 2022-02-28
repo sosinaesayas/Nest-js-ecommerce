03af773a26e6cb66d3a126390f6e513555a057f4
sosinaesayas <sosinaesayas@gmail.com>
2022-02-28 17:08:07 -0300

    Add class validator in my CreateUSerDTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,17 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class CreateUserDTO {
+  @IsNotEmpty()
+  @Length(8, 15)
   username: string;
+
+  @IsNotEmpty()
+  @Length(12, 30)
   password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
-  role: string;
 }
 
 export type UpdateUserDTO = Partial<CreateUserDTO>;
