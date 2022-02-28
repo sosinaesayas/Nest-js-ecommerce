091dfb253792b58153313f4dba5f5ac673b74082
sosinaesayas <sosinaesayas@gmail.com>
2022-02-28 17:09:54 -0300

    Delete role on my User interface

--- src/user/interfaces/user.interface.ts
+++ src/user/interfaces/user.interface.ts
@@ -1,11 +1,9 @@
 import { Document } from 'mongoose';
 
 export interface User extends Document {
-  _id: string;
   username: string;
   password: string;
   email: string;
-  role: string;
   state: boolean;
   createdAt?: Date;
 }
