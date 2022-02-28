6e8ff21c4c24053afd8d98743f2d0e3d2be470d9
sosinaesayas <sosinaesayas@gmail.com>
2022-02-28 17:07:41 -0300

    Add class validator in my CreateProductDTO

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,11 +1,34 @@
+import { IsNotEmpty, IsNumber, Length } from 'class-validator';
 export class CreateProductDTO {
+  @IsNotEmpty()
+  @Length(8, 20)
   title: string;
+
+  @IsNotEmpty()
   image: string;
+
+  @IsNotEmpty()
+  @Length(25, 250)
   description: string;
+
+  @IsNotEmpty()
+  @Length(3, 15)
   brand: string;
+
+  @IsNotEmpty()
+  @Length(3, 20)
   category: string;
+
+  @IsNotEmpty()
+  @Length(3, 20)
   subcategory: string;
+
+  @IsNotEmpty()
+  @Length(30, 180)
   comment: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
 }
 
