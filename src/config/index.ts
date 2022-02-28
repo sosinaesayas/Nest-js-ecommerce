b01378531b8a34b3e011879ba6eabe967a68a8f7
sosinaesayas <sosinaesayas@gmail.com>
2022-02-28 17:10:26 -0300

    Add news envs on my Config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,22 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const { MONGO_URL, SECRET_KEY, PORT } = process.env;
+const {
+  MONGO_URL,
+  SECRET_KEY,
+  PORT,
+  MAIL_HOST,
+  MAIL_USER,
+  MAIL_PASSWORD,
+  MAIL_FROM,
+} = process.env;
 
 export const config = {
   mongoURL: MONGO_URL,
   jwt: SECRET_KEY,
   port: PORT,
+  mailSMPT: MAIL_HOST,
+  mailUser: MAIL_USER,
+  mailPassword: MAIL_PASSWORD,
+  mailFrom: MAIL_FROM,
 };
