6f965c46b50e626b0eaac5bc70086a6c2576c39c
sosinaesayas <sosinaesayas@gmail.com>
2022-03-07 16:32:47 -0300

    Feat: Implements RegisterUser on my AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,35 @@
-import { Controller, Post, Request, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { RegisterUserDTO } from 'src/user/dto/register-user.dto';
+import { User } from 'src/user/schemas/user.schema';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async create(
+    @Res() res,
+    @Body() registerUserDTO: RegisterUserDTO,
+  ): Promise<User> {
+    try {
+      const user = await this.authService.registerUser(registerUserDTO);
+      return res.json({
+        message: 'User created successfully',
+        user,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
