2411fd269b9b6dd0b80534057f73f958d791be5c
sosinaesayas <sosinaesayas@gmail.com>
2022-03-07 16:34:02 -0300

    Feat: Implements Seller Guard

---
+++ src/guards/seller.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ERROR_MESSAGES } from '../shared/ERROR_MESSAGES';
+
+@Injectable()
+export class SellerGuard implements CanActivate {
+  constructor() {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (user.seller) {
+      return true;
+    }
+
+    throw new HttpException(
+      ERROR_MESSAGES.UNAUTHORIZE,
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+}
