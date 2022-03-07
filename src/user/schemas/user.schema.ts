f5a83e89d18a85e93683b15c6386e511c85d1d25
sosinaesayas <sosinaesayas@gmail.com>
2022-03-07 16:29:43 -0300

    Add isEmailconfirmed property in my User Schema

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -20,6 +20,9 @@ export class User {
   @Prop({ type: Boolean, required: false })
   state: boolean;
 
+  @Prop({ type: Boolean })
+  isEmailConfirmed: boolean;
+
   @Prop({ type: Date, default: Date.now })
   createdAt: Date;
 }
